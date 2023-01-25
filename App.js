93bee261cc191c814c22309e14c8760f76802640
Romain Noeppel <romain_noeppel@hotmail.com>
2023-01-26 00:43:42 +0100

    update

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
-import { StyleSheet, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import  Navigation from './Components/Navigation';
 import React from "react";
+import database from '@react-native-firebase/database';
 
+//database()
+  //.ref('https://ebb-app-32a47-default-rtdb.europe-west1.firebasedatabase.app/')
+  //.once('value')
+  //.then(snapshot => {
+    //console.log('User data: ', snapshot.val());
+  //});
 
 export default function App() {
   return (
